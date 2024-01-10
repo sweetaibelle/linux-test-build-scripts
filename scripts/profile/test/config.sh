8298300c804725a8c1909999648a9dfbf1877778
SweetAI Belle <SweetAIBelle@gmail.com>
2024-01-09 22:42:35 -0800

    Add test profile that's mostly a copy of chonky at the moment.

---
+++ scripts/profile/test/config.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+# Helper variables and functions specific to this profile.
+
+# Image settings
+image_size=1000000 # 1 GB
+image_name=test.img
+
+# Directory to build in
+base_dir=test
